c5f7706138b1629db4010d4aaac710c26b440139
Magloire Sendegeya <mase@edc.dk>
2020-09-08 10:47:14 +0200

    Updates

--- precache-manifest.e18620a19f8bcb9052ba2a68b3bea162.js
+++ precache-manifest.e18620a19f8bcb9052ba2a68b3bea162.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b53fded8162684fdb49a56723942bdcd",
+    "revision": "1a5638dd2591416fcf7cdfcb37478d05",
     "url": "/cvrflytny/index.html"
   },
   {
-    "revision": "133e3d94a83ac2e1f767",
+    "revision": "d66fc3fa9bc76c82ef81",
     "url": "/cvrflytny/static/css/main.5e4e9510.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/cvrflytny/static/js/2.a97ff339.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "133e3d94a83ac2e1f767",
-    "url": "/cvrflytny/static/js/main.05092b81.chunk.js"
+    "revision": "d66fc3fa9bc76c82ef81",
+    "url": "/cvrflytny/static/js/main.f5360998.chunk.js"
   },
   {
     "revision": "00c82f782188f056c3e5",
